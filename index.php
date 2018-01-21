cd8403a0da7b31be4216243e93994b3ea707e03f
Javier García <javiergc01@gmail.com>
2018-01-21 15:05:09 +0100

    New index.php

--- index.php
+++ index.php
@@ -1,43 +1,19 @@
 <html>
 <head>
-	<title>StreamMusic | Add new song</title>
+	<title>StreamMusic</title>
 	<link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
-	<div class="container">
-		<div id="contact">
-			<h3>StreamMusic</h3>
-			<?php
-				$path = '/var/www/javgarcal.com/public_html/streammusic/files/';
-				$files = scandir($path);
-				
-				$i = 0;
-				foreach($files as $f) {
-					if($i++ < 2) {
-						continue;
-					}
-					echo $f, '<br>';
-					$albums = scandir($path . '' . $f . '/');
-					$ii = 0;
-					foreach($albums as $a) {
-						if($ii++ < 2) {
-							continue;
-						}
-						echo $a, '<br>';
-						$songs = scandir($path . '' . $f . '/' . $a . '/');
-						$iii = 0;
-						foreach($songs as $s) {
-							if($iii++ < 2) {
-								continue;
-							}
-							echo '<a href="files/'.$f.'/'.$a.'/'.$s.'">'.$s.'</a>', '<br>';
-						}
-					}
-					echo '<hr/>';
-				}
-			?>
-			<p class="copyright">StreamMusic</p>
-		</div>
+	<div class="container">  
+	  <form id="contact" action="addsong.php" method="post">
+		<h3>Nueva lista</h3>
+		<h4>StreamMusic</h4>
+		<textarea placeholder="Escribe aquí las canciones que quieras, una por línea." name="songs" cols="40" rows="5" required autofocus></textarea>
+		
+		<button name="submit" type="submit" id="contact-submit" data-submit="Procesando...">Aceptar</button>
+		</fieldset>
+		<p class="copyright">StreamMusic</p>
+	  </form>
 	</div>
 </body>
 </html>
